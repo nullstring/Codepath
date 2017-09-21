c774f4b70d64da46356fc2683df6bc78d3c9baba
Harsh Mehta <harsh.version.1@gmail.com>
2017-09-20 19:10:45 -0700

    Github init

--- Yelp/Yelp/BusinessesViewController.swift
+++ Yelp/Yelp/BusinessesViewController.swift
@@ -8,20 +8,34 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
+    var businessSearchBar: UISearchBar!
     @IBOutlet weak var businessesTableView: UITableView!
     var businesses: [Business] = []
+    var filteredBusinesses: [Business] = []
+
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadSearchBar()
+        
         businessesTableView.dataSource = self
         businessesTableView.delegate = self
+        businessesTableView.rowHeight = UITableViewAutomaticDimension
+        businessesTableView.estimatedRowHeight = 200
+        
+
+        
+        filteredBusinesses = businesses
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses!
+            self.filteredBusinesses = businesses!
+            
             self.businessesTableView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
@@ -53,13 +67,49 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return businesses.count
+        return filteredBusinesses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessTableViewCell", for: indexPath) as! BusinessTableViewCell
-        cell.business = businesses[indexPath.row]
+        cell.business = filteredBusinesses[indexPath.row]
         return cell
+    }
+    
+    // MARK: - Search bar
+    fileprivate func loadSearchBar() {
+        businessSearchBar = UISearchBar()
+        businessSearchBar.sizeToFit()
+        businessSearchBar.delegate = self
+        navigationItem.titleView = businessSearchBar
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        businessSearchBar.showsCancelButton = false
+        businessSearchBar.text = ""
+        businessSearchBar.endEditing(true)
+        filteredBusinesses = businesses
+        businessesTableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        businessSearchBar.showsCancelButton = true
+    }
+    
+    // This method updates filteredData based on the text in the
+    // Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        filteredBusinesses = searchText.isEmpty ? businesses : businesses.filter { (item: Business) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.name!.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        businessesTableView.reloadData()
     }
     
     /*
